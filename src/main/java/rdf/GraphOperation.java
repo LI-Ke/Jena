758e13c6ad831b7008872ff15f8fcea5fb4397b6
Ke Li <li_ke@yahoo.com>
2017-03-28 15:56:53 +0200

    Add files via upload

---
+++ src/main/java/rdf/GraphOperation.java
@@ -0,0 +1,42 @@
+package rdf;
+
+import java.io.InputStream;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.FileManager;
+
+public class GraphOperation {
+
+	public static void main(String[] args) {
+
+	    final String inputFileName1 = "vc-db-3.rdf";    
+	    final String inputFileName2 = "vc-db-4.rdf";
+	    
+	    // create an empty model
+        Model model1 = ModelFactory.createDefaultModel();
+        Model model2 = ModelFactory.createDefaultModel();
+        
+        // use the class loader to find the input file
+        InputStream in1 = FileManager.get().open(inputFileName1);
+        if (in1 == null) {
+            throw new IllegalArgumentException( "File: " + inputFileName1 + " not found");
+        }
+        InputStream in2 = FileManager.get().open(inputFileName2);
+        if (in2 == null) {
+            throw new IllegalArgumentException( "File: " + inputFileName2 + " not found");
+        }
+        
+        // read the RDF/XML files
+        model1.read( in1, "" );
+        model2.read( in2, "" );
+        
+        // merge the graphs
+        Model model = model1.union(model2);
+        
+        // print the graph as RDF/XML
+        model.write(System.out, "RDF/XML-ABBREV");
+        System.out.println();
+	}
+
+}
